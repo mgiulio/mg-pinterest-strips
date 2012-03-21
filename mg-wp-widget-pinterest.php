dd5d4dfc8bf2b6ba0ce5a9aa2903496732309ba4
mgiulio <giulio.mainardi@gmail.com>
2012-03-21 15:11:00 +0100

    buildPinboard_* test functions

--- mg-wp-widget-pinterest.php
+++ mg-wp-widget-pinterest.php
@@ -106,7 +106,15 @@ class mg_Widget_Pinterest extends WP_Widget {
 
 		echo $before_widget;
 		echo $before_title . $title . $after_title;
-		
+		$this->buildPinboard($rss);
+		//$this->buildPinboard_noBorders($rss);
+		echo $after_widget;
+
+		$rss->__destruct();
+		unset($rss);
+	}
+	
+	function buildPinboard($rss) {
 		$pinWidth = 50;
 		$numCols = 4;
 		$margin = 1;
@@ -141,21 +149,60 @@ class mg_Widget_Pinterest extends WP_Widget {
 			imagejpeg($thumbIm, $thumbUrl);
 			$i++;
 		}
-		/* echo "<div class='pinboard' style='width: {$pinboardInnerWidth}px; margin: 10px auto; padding: {$margin}px; background-color: #000;'>";
-		foreach ($cols as $i => $c) {
-			echo "<div class='col' style='width: " . ($i < $numCols-1 ? $colWidth : $colWidth-1) . "px; float: left; margin: 0; padding: 0'>";
-			echo implode('', $c);
-			echo "</div>";
-		}
-		echo "<div style='clear: both;'>&nbsp;</div>";
-		echo "</div>"; */
-		
-		echo $after_widget;
-
-		$rss->__destruct();
-		unset($rss);
+		echo "<div class='pinboard' style='width: {$pinboardInnerWidth}px; margin: 10px auto; padding: {$margin}px; background-color: none;'>";
+			foreach ($cols as $i => $c) {
+				echo "<div class='col' style='width: " . ($i < $numCols-1 ? $colWidth : $colWidth-1) . "px; float: left; margin: 0; padding: 0'>";
+				echo implode('', $c);
+				echo "</div>";
+			}
+			echo "<div style='clear: both;'>&nbsp;</div>";
+		echo "</div>";
 	}
 	
+	function buildPinboard_noBorders($rss) {
+		$pinWidth = 50;
+		$numCols = 4;
+		$margin = 1;
+		
+		$colWidth = $pinWidth + $margin;
+		$pinboardInnerWidth = $colWidth*$numCols - 2*$marging-$margin;
+		
+		$cols = array();
+		$c = 0;
+		$i = 0;
+		foreach ($rss->get_items(0, $instance['items']) as $item) {
+			$title = esc_attr(strip_tags($item->get_title()));
+			$link = $item->get_link();
+			$desc = $item->get_description();
+			$imgSrc = array();
+			preg_match('/src="([^"]+)"/', $desc, $imgSrc);
+			//preg_match('/src="(.*)"/', $desc, $imgSrc);
+			$imgUrl = $imgSrc[1];
+			
+			$cols[$c][] = "<a href='$link'><img style='max-width: none; display: block; width: {$pinWidth}px; margin: 0; padding: 0; margin-bottom: {$margin}px;' src='$imgUrl' title='$title' alt='$title'></a>";
+			$c = ($c+1) % $numCols;
+			
+			$pinIm = imagecreatefromjpeg($imgUrl);
+			$pinW = imagesx($pinIm);
+			$pinH = imagesy($pinIm);
+			$pinAspectRatio = $pinW / (float)$pinH;
+			$thumbW = $pinWidth;
+			$thumbH = $thumbW / $pinAspectRatio;
+			$thumbIm = imagecreatetruecolor($thumbW, $thumbH);
+			imagecopyresized($thumbIm, $pinIm, 0, 0, 0, 0, $thumbW, $thumbH, $pinW, $pinH);
+			$thumbUrl = $mg_pin_plugin_dir . "thumb-{$i}.jpg";
+			imagejpeg($thumbIm, $thumbUrl);
+			$i++;
+		}
+		echo "<div class='pinboard' style='width: {$pinboardInnerWidth}px; margin: 10px auto; padding: {$margin}px; background-color: none;'>";
+			foreach ($cols as $i => $c) {
+				echo "<div class='col' style='width: " . ($i < $numCols-1 ? $colWidth : $colWidth-1) . "px; float: left; margin: 0; padding: 0'>";
+				echo implode('', $c);
+				echo "</div>";
+			}
+			echo "<div style='clear: both;'>&nbsp;</div>";
+		echo "</div>";
+	}
 }
 
 add_action('widgets_init', create_function('', 'register_widget("mg_Widget_Pinterest");'));
