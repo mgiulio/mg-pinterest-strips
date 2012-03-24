a707584257ea141805da3cc0d7ea61c6952663af
mgiulio <giulio.mainardi@gmail.com>
2012-03-24 10:36:46 +0100

    Implementation with SimpleXML

--- mg-wp-widget-pinterest.php
+++ mg-wp-widget-pinterest.php
@@ -10,58 +10,6 @@ Author URI: http://mgiulio.altervista.org
 License: GPL2
 */
 
-require_once (ABSPATH . WPINC . '/class-feed.php');
-class SimplePie_Cache_Extras extends SimplePie {
-	function get_cache_object() {
-		$cache = call_user_func(array($this->cache_class, 'create'), $this->cache_location, call_user_func($this->cache_name_function, $this->feed_url), 'spc');
-		return $cache;
-	}
- 
-	function get_cache_filename() {
-		$cache = $this->get_cache_object();
-		return $cache->name;
-	}
- 
-	function get_cache_timestamp() {
-		$cache = $this->get_cache_object();
-		return $cache->mtime();
-	}
- 
-	function get_cache_time_remaining($format = false) {
-		$cache = $this->get_cache_object();
-		$remaining = ($cache->mtime() + $this->cache_duration) - time();
- 
-		if ($format)
-		{
-			return SimplePie_Misc::time_hms($remaining);
-		}
-		else
-		{
-			return $remaining;
-		}
-	}
-}
-
-function mg_fetch_feed($url, /* $cache_life, */ &$feed) {
-	require_once (ABSPATH . WPINC . '/class-feed.php');
-
-	$feed = new SimplePie_Cache_Extras();
-	$feed->set_feed_url($url);
-	$feed->set_cache_class('WP_Feed_Cache');
-	$feed->set_file_class('WP_SimplePie_File');
-	$feed->set_cache_duration(60/* $cache_life *//* apply_filters('wp_feed_cache_transient_lifetime', 43200, $url) */);
-	//do_action_ref_array( 'wp_feed_options', array( &$feed, $url ) );
-	
-	$is_cache_expired = $feed->get_cache_time_remaining() < 0;
-	$feed->init();
-	$feed->handle_content_type();
-
-	if ( $feed->error() )
-		return new WP_Error('simplepie-error', $feed->error());
-
-	return $is_cache_expired;
-}
-
 class mg_Widget_Pinterest extends WP_Widget {
 	function __construct() {
 		parent::__construct(
@@ -77,6 +25,14 @@ class mg_Widget_Pinterest extends WP_Widget {
 		if (!file_exists($this->cache_dir))
 			mkdir($this->cache_dir);
 		$this->cache_url = $this->plugin_url . 'cache/';
+	}
+	
+	function get_markup_path() {
+		return $this->cache_dir . "markup-{$this->number}.html";
+	}
+	
+	function get_sprite_path() {
+		return $this->cache_dir . "sprite-{$this->number}.jpg";
 	}
 	
 	function form($instance) {
@@ -156,12 +112,8 @@ class mg_Widget_Pinterest extends WP_Widget {
 		$instance['cache_life'] = strip_tags($new_instance['cache_life']);
 		
 		$feed_url = "http://pinterest.com/{$instance['username']}/feed.rss";
-		$rss = NULL;
-		mg_fetch_feed($feed_url, $rss);
-		if (is_wp_error($rss))
-			return;
 		ob_start();
-		$this->build_pinboard_no_borders_sprite($rss, $instance['strip_width'], $instance['num_strips']);
+		$this->build_pinboard($this->fetch_feed($feed_url), $instance['strip_width'], $instance['num_strips']);
 		fwrite(fopen($this->cache_dir . "markup-{$this->number}.html", 'w'), ob_get_contents());
 		ob_end_clean();
 		
@@ -176,10 +128,6 @@ class mg_Widget_Pinterest extends WP_Widget {
 			return;
 		
 		$feed_url = "http://pinterest.com/$username/feed.rss";		
-		$rss = NULL;
-		$is_fresh = mg_fetch_feed($feed_url, $rss);
-		if (is_wp_error($rss))
-			return;
 		
 		$title = apply_filters('widget_title', $instance['title']);
 		$title = 
@@ -190,120 +138,25 @@ class mg_Widget_Pinterest extends WP_Widget {
 		echo $before_widget;
 		echo $before_title . $title . $after_title;
 		
-		if ($is_fresh || 
-			!file_exists($this->cache_dir . "markup-{$this->number}.html") ||
-			!file_exists($this->cache_dir . "sprite-{$this->number}.jpg")
-		) {
+		if ($this->must_regenerate_cache()) {
 			ob_start();
-			$this->build_pinboard_no_borders_sprite($rss, $instance['strip_width'], $instance['num_strips']);
-			fwrite(fopen($this->cache_dir . "markup-{$this->number}.html", 'w'), ob_get_contents());
+			$this->build_pinboard($this->fetch_feed($feed_url), $instance['strip_width'], $instance['num_strips']);
+			fwrite(fopen($this->get_markup_path(), 'w'), ob_get_contents());
 			ob_end_flush();
 		}
 		else
-			readfile($this->cache_dir . "markup-{$this->number}.html");
+			readfile($this->get_markup_path());
 		
 		echo $after_widget;
-
-		$rss->__destruct();
-		unset($rss);
 	}
 	
-	function buildPinboard($rss) {
-		$pinWidth = 50;
-		$numCols = 4;
-		$margin = 1;
-		
-		$colWidth = $pinWidth + $margin;
-		$pinboard_inner_width = $colWidth*$numCols - 2*$marging-$margin;
-		
-		$cols = array();
-		$c = 0;
-		$i = 0;
-		foreach ($rss->get_items(0, $instance['items']) as $item) {
-			$title = esc_attr(strip_tags($item->get_title()));
-			$link = $item->get_link();
-			$desc = $item->get_description();
-			$imgSrc = array();
-			preg_match('/src="([^"]+)"/', $desc, $imgSrc);
-			//preg_match('/src="(.*)"/', $desc, $imgSrc);
-			$imgUrl = $imgSrc[1];
-			
-			$cols[$c][] = "<a href='$link'><img style='max-width: none; display: block; width: {$pinWidth}px; margin: 0; padding: 0; margin-bottom: {$margin}px;' src='$imgUrl' title='$title' alt='$title'></a>";
-			$c = ($c+1) % $numCols;
-			
-			$pinIm = imagecreatefromjpeg($imgUrl);
-			$pinW = imagesx($pinIm);
-			$pinH = imagesy($pinIm);
-			$pinAspectRatio = $pinW / (float)$pinH;
-			$thumbW = $pinWidth;
-			$thumbH = $thumbW / $pinAspectRatio;
-			$thumbIm = imagecreatetruecolor($thumbW, $thumbH);
-			imagecopyresized($thumbIm, $pinIm, 0, 0, 0, 0, $thumbW, $thumbH, $pinW, $pinH);
-			$thumbUrl = $this->plugin_dir . "thumb-{$i}.jpg";
-			imagejpeg($thumbIm, $thumbUrl);
-			$i++;
-		}
-		echo "<div class='pinboard' style='width: {$pinboard_inner_width}px; margin: 10px auto; padding: {$margin}px; background-color: none;'>";
-			foreach ($cols as $i => $c) {
-				echo "<div class='col' style='width: " . ($i < $numCols-1 ? $colWidth : $colWidth-1) . "px; float: left; margin: 0; padding: 0'>";
-				echo implode('', $c);
-				echo "</div>";
-			}
-			echo "<div style='clear: both;'>&nbsp;</div>";
-		echo "</div>";
-	}
-	
-	function buildPinboard_noBorders($rss) {
-		$pinWidth = 50;
-		$numCols = 4;
-		
-		$colWidth = $pinWidth;
-		$pinboard_inner_width = $colWidth*$numCols;
-		
-		$cols = array();
-		$c = 0;
-		$i = 0;
-		foreach ($rss->get_items(0, $instance['items']) as $item) {
-			$title = esc_attr(strip_tags($item->get_title()));
-			$link = $item->get_link();
-			$desc = $item->get_description();
-			$imgSrc = array();
-			preg_match('/src="([^"]+)"/', $desc, $imgSrc);
-			//preg_match('/src="(.*)"/', $desc, $imgSrc);
-			$imgUrl = $imgSrc[1];
-			
-			$cols[$c][] = "<a href='$link'><img style='max-width: none; display: block; width: {$pinWidth}px; margin: 0; padding: 0; margin-bottom: {$margin}px;' src='$imgUrl' title='$title' alt='$title'></a>";
-			$c = ($c+1) % $numCols;
-			
-			$pinIm = imagecreatefromjpeg($imgUrl);
-			$pinW = imagesx($pinIm);
-			$pinH = imagesy($pinIm);
-			$pinAspectRatio = $pinW / (float)$pinH;
-			$thumbW = $pinWidth;
-			$thumbH = $thumbW / $pinAspectRatio;
-			$thumbIm = imagecreatetruecolor($thumbW, $thumbH);
-			imagecopyresized($thumbIm, $pinIm, 0, 0, 0, 0, $thumbW, $thumbH, $pinW, $pinH);
-			$i++;
-		}
-		echo "<div class='pinboard' style='width: {$pinboard_inner_width}px; margin: 10px auto; padding: 0px; background-color: none;'>";
-			foreach ($cols as $i => $c) {
-				echo "<div class='col' style='width: {$pinWidth}px; float: left; margin: 0; padding: 0'>";
-				echo implode('', $c);
-				echo "</div>";
-			}
-			echo "<div style='clear: both;'>&nbsp;</div>";
-		echo "</div>";
-	}
-	
-	function build_pinboard_no_borders_sprite($rss, $stripW, $numStrips) {
+	function build_pinboard($pins, $stripW, $numStrips) {
 		$pinboard_inner_width = $stripW * $numStrips;
-		
-		$items = $rss->get_items(0, $instance['items']);
 		
 		// Compute the sprite height and allocate it
 		$spriteH = 0;
-		foreach ($items as $item) {
-			$im = imagecreatefromjpeg($this->get_image_url($item->get_description()));
+		foreach ($pins as $pin) {
+			$im = imagecreatefromjpeg($pin['image_url']);
 			
 			$w = imagesx($im);
 			$h = imagesy($im);
@@ -320,11 +173,7 @@ class mg_Widget_Pinterest extends WP_Widget {
 		$cols = array();
 		$c = 0;
 		$y = 0;
-		foreach ($items as $item) {
-			// Get item info
-			$title = esc_attr(strip_tags($item->get_title()));
-			$link = $item->get_link();
-			
+		foreach ($pins as $pin) {
 			// Make thumbnail and append it to the sprite
 			$currIm = array_shift($pinIm);
 			$thumb_h = $currIm['thumb_h'];
@@ -332,13 +181,13 @@ class mg_Widget_Pinterest extends WP_Widget {
 			imagedestroy($currIm['im']);
 			
 			// Generate the markup for this item
-			$cols[$c][] = "<a href='$link' title='$title' style='display: block; width: {$stripW}px; height: {$thumb_h}px; margin: 0; padding: 0; background: url($spriteUrl) no-repeat 0 -{$y}px; text-indent: -9999px;'>$title</a>";
+			$cols[$c][] = "<a href='{$pin['link']}' title='$title' style='display: block; width: {$stripW}px; height: {$thumb_h}px; margin: 0; padding: 0; background: url($spriteUrl) no-repeat 0 -{$y}px; text-indent: -9999px;'>{$pin['title']}</a>";
 			$c = ($c+1) % $numStrips;
 			
 			$y += $thumb_h;
 		}
 		// Save the sprite
-		imagejpeg($spriteIm, $this->cache_dir . "sprite-{$this->number}.jpg");
+		imagejpeg($spriteIm, $this->get_sprite_path());
 		imagedestroy($spriteIm);
 		
 		echo "<div class='pinboard' style='width: {$pinboard_inner_width}px; margin: 10px auto; padding: 0px; background-color: none;'>";
@@ -355,6 +204,32 @@ class mg_Widget_Pinterest extends WP_Widget {
 		$imgSrc = array();
 		preg_match('/src="([^"]+)"/', $itemDesc, $imgSrc);
 		return $imgSrc[1];
+	}
+	
+	function must_regenerate_cache() {
+		$last_build_timestamp = filemtime($this->get_markup_path()); //http://it2.php.net/manual/en/function.clearstatcache.php
+		$cache_lifetime = 120;
+
+		return
+			$last_build_timestamp + $cache_lifetime <= time() ||
+			!file_exists($this->get_markup_path()) ||
+			!file_exists($this->get_sprite_path())
+		;
+	}
+	
+	function fetch_feed($url) {
+		$rss = simplexml_load_file($url);
+		// Eorhandling
+
+		$pins = array();
+		foreach ($rss->channel->item as $item)
+			$pins[] = array(
+				'title' => (string)$item->title,//$title = esc_attr(strip_tags($item->get_title()));
+				'link' =>(string)$item->link,
+				'image_url' => $this->get_image_url((string)$item->description)
+			);
+		
+		return $pins;
 	}
 }
 
